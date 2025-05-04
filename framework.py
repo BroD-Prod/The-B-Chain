2f2c6d6d0302cc9f6fe6942bacdb2bfca3ba03c5
BroD-Prod <brodyroth23@gmail.com>
2025-05-04 15:44:59 -0400

    Added functionality to mine and transaction route

--- framework.py
+++ framework.py
@@ -1,6 +1,5 @@
 from uuid import uuid4
-from flask import Flask, jsonify
-from textwrap import dedent
+from flask import Flask, jsonify, request
 from blockchain import Blockchain
 
 app = Flask(__name__)
@@ -11,11 +10,40 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return "Mine a new block"
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    blockchain.new_transaction(
+        sender=0,
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof,previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash']
+    }
+    return jsonify(response),200
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    return "Add A New Transaction"
+    values = request.get_json()
+
+    required = ['sender','receiver','amount']
+    if not all(k in values for k in required):
+        return 'Missing Value', 400
+
+    index = blockchain.new_transaction(values['sender'],values['reciever'],values['amount'])
+
+    response = {'message': f'Transaction added to Block {index}'}
+    return jsonify(response), 201
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
