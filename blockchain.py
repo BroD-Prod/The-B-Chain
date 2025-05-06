06d9f8f67b07a03454c3553d50aff51d0690e622
BroD-Prod <brodyroth23@gmail.com>
2025-05-06 15:32:10 -0400

    Adding node registration and conflict resolution for the Consensus Algorithm
    This creates the Conses Algorithm to allow for decentralization on this blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,14 +1,78 @@
 import hashlib
 import json
 from time import time
+from urllib.parse import urlparse
+import requests
 
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.nodes = set()
 
         #Gensis Block
         self.new_block(previous_hash=1, proof=100)
+
+    def register_node(self, address):
+        """
+        :param address: <str> Address of node
+        :return:
+        """
+
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def valid_chain(self, chain):
+        """
+        :param chain: <list> A blockchain
+        :return: <bool> True if valid
+        """
+
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n----------\n")
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts(self):
+        """
+        :return: <bool> True if our chain was replaced
+        """
+
+        neighbours = self.nodes
+        new_chain = None
+
+        max_length = len(self.chain)
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                #Check if length is longer and if chain is valid
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
 
     def new_block(self, proof, previous_hash=None):
         """
